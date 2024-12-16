ad28829e7d682915ccf2dcebbc383c8da825d11f
mia-OwO <Ruing.OqO@gmail.com>
2024-12-16 13:21:54 +0900

    commit8

--- src/component/PhotoUploader.js
+++ src/component/PhotoUploader.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+//파일 ->URL
+//onImageUploade: imgID,URL 전달
+//inputRef:input에 접근
 const PhotoUploader = ({ onImageUpload, inputRef }) => {
   const [fileCounter, setFileCounter] = useState(1); // 파일 번호를 관리하는 상태
 
@@ -8,7 +10,7 @@ const PhotoUploader = ({ onImageUpload, inputRef }) => {
     const file = e.target.files[0]; // 사용자가 업로드한 첫 번째 파일 가져오기
     if (file) {
       // 현재 날짜를 기반으로 고유한 파일 이름 생성
-      const now = new Date();
+      const now = new Date();   // 연도 -> 문자열 -> 두자리만 , 월 + 1-> 문자열 -> 두자리, 한자리면 앞에 0
       const dateString = `${now.getFullYear().toString().slice(2)}${(now.getMonth() + 1)
         .toString()
         .padStart(2, "0")}${now.getDate().toString().padStart(2, "0")}`; // YYMMDD 형식
@@ -18,7 +20,7 @@ const PhotoUploader = ({ onImageUpload, inputRef }) => {
 
       // FileReader를 사용하여 파일 데이터를 읽어 브라우저에서 표시 가능한 URL 생성
       const reader = new FileReader();
-      reader.onload = () => {
+      reader.onload = () => {  //파일 읽기가 완료되면 실행 reader.result에 데이터 저장
         // 부모 컴포넌트로 이미지 정보 전달
         onImageUpload({ imgId, dataUrl: reader.result });
       };
@@ -29,15 +31,15 @@ const PhotoUploader = ({ onImageUpload, inputRef }) => {
   return (
     <div className="PhotoUploader"> {/* 컨테이너 스타일 클래스 */}
       <input 
-        ref={inputRef}
+        ref={inputRef} //inputRef를 통해 dom요소 접근
         type="file" 
-        accept="image/*" 
+        accept="image/*"  //이미지 파일만
         onChange={handleImageUpload} 
-        style={{display:"none"}}
+        style={{display:"none"}} //버튼만 입력필드x
       /> {/* 이미지 파일 선택 */}
 
       <button
-      onClick={() => inputRef?.current?.click()}
+      onClick={() => inputRef?.current?.click()} // 파일창 열ㄹ기, inputRef.current 존재 x: click() 실행 x 있으면 실행 
       style={{cursor:"pointer"}} />
 
     </div>
